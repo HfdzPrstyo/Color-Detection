8ba53aa8b6ee2d6729582ba293abad76b38530aa
HfdzPrstyo <hafidzprasetyo160@gmail.com>
2025-12-16 12:58:28 +0700

    fix deploy 2

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ CORS(app)
 
 # Get paths
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-MODELS_DIR = os.path.join(BASE_DIR, 'models')
+# MODELS_DIR = os.path.join(BASE_DIR, 'models')
+MODELS_DIR = 'models'
 
 # Load model dan scaler dari models folder
 print("Loading model and scaler...")
